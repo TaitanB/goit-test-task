1e55ba743ed0f2c64664750d7ddac4aea2007a41
Taitan <110903767+TaitanB@users.noreply.github.com>
2023-05-18 22:06:17 +0300

    Updates

---
+++ assets/Home-3f15fa74.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-2a8096aa.js";function r(){return e.jsx("div",{children:e.jsx("h1",{children:"Following Net"})})}export{r as default};
---
+++ assets/picture-1ae1ff0b.js
@@ -0,0 +1,1 @@
+const t="/goit-test-task/assets/picture-9f4b25d6.png";export{t as p};
