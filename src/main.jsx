943740137a90b35218199776ada29e31214e5f41
Taitan <110903767+TaitanB@users.noreply.github.com>
2023-05-20 09:50:31 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="goit-test-task">
+    <BrowserRouter basename="/goit-test-task">
       <App />
     </BrowserRouter>
   </React.StrictMode>
